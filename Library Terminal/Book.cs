20c61f003e660b5ab64534bbf80c75eb660a8c31
jstrathres <jstraz7@gmail.com>
2023-01-26 14:08:11 -0500

    class, lists work

---
+++ Library Terminal/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Terminal
+{
+    public class Book
+    {
+        //properties
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public bool Status { get; set; }
+        public DateTime DueDate { get; set; }   //maybe remove int
+
+
+        public Book(string _title, string _author, bool _status, DateTime _dueDate)
+        {
+            Title = _title;
+            Author = _author;
+            Status = _status;
+            DueDate = _dueDate;
+        }
+    }
+}
--- Library Terminal/Program.cs
+++ Library Terminal/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using Library_Terminal;
+List<Book> books = new List<Book>()
+{
+new Book("Rocky", "Stallone", true, DateTime.Now),
+new Book("r", "r", true, DateTime.Now),
+//new Book("", "", , ),
+//new Book("", "", , ),
+//new Book("", "", , ),
+//new Book("", "", , )
+//new Book("", "", , )
+//new Book("", "", , )
+//new Book("", "", , )
+//new Book("", "", , )
+//new Book("", "", , )
+//new Book("", "", , )
+//new Book("", "", , )
+};
+
+//Display all Books
+foreach (Book b in books)
+{
+    Console.WriteLine($"{b.Title}|{b.Author}|{b.DueDate}");
+}
+
+//Search by author
+
+//Search by title keyword
