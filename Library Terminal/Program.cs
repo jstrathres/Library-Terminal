d6837d604e7e8119bbada63a586db645c89c2e83
Junyoung Kim <jkim5929@gmail.com>
2023-01-26 14:24:49 -0500

    Added a search menu

--- Library Terminal/Program.cs
+++ Library Terminal/Program.cs
@@ -1,4 +1,8 @@
 ﻿using Library_Terminal;
+
+bool runProgram = true;
+
+
 List<Book> books = new List<Book>()
 {
 new Book("Rocky", "Stallone", true, DateTime.Now),
@@ -16,12 +20,53 @@ new Book("r", "r", true, DateTime.Now),
 //new Book("", "", , )
 };
 
-//Display all Books
-foreach (Book b in books)
+
+while (runProgram)
 {
-    Console.WriteLine($"{b.Title}|{b.Author}|{b.DueDate}");
+    Console.WriteLine("Welcome to the GC library. Please select an option");
+    Console.WriteLine("1. View all books");
+    Console.WriteLine("2. Search by author");
+    Console.WriteLine("3. Search by title");
+
+    int choice = int.Parse(Console.ReadLine());
+    if (choice ==1)
+    {
+        //Display all Books
+        foreach (Book b in books)
+        {
+            Console.WriteLine($"{b.Title}|{b.Author}|{b.DueDate}");
+        }
+    }
+    else if (choice ==2)
+    {
+        //Search by author
+        Console.Write("Please enter the author: ");
+        string author = Console.ReadLine().ToLower().Trim();
+        foreach (Book b in books.Where(b => b.Author.ToLower() == author))
+        {
+            Console.WriteLine($"{b.Title}");   
+        }
+    }
+    else if (choice ==3)
+    {
+        //Search by title keyword
+    }
+
+
 }
 
-//Search by author
 
-//Search by title keyword
+
+
+
+
+
+
+
+
+
+
+
+
+
+
